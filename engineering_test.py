b4e7bbbfd927d658fbfe09286ad767c4e14db819
ptejasri <39000766+ptejasri@users.noreply.github.com>
2022-08-25 10:53:32 +0530

    Add files via upload

--- engineering_test.py
+++ engineering_test.py
@@ -3,23 +3,26 @@ import sys
 import pandas as pd
 
 def process_input_csv(filepath):
-    dataframes = []
-    for file in os.listdir(filepath):
-        if str(file).endswith('csv'):
-            if not str(file).lower().startswith('combined'):
-                environment = ''
-                if str(file).lower().startswith('na'):
-                    environment = 'NA Prod'
-                elif str(file).lower().startswith('asia'):
-                    environment = 'Asia Prod'
-                df = pd.read_csv(os.path.join(filepath, file), usecols=['Source IP'])
-                df['Environment'] = environment
-                dataframes.append(df)
-    data_concated = pd.concat(dataframes, axis=0)
-    data_concated = data_concated.reset_index()
-    data_concated = data_concated.drop(['index'], axis=1)
-    data_concated = data_concated.drop_duplicates()
-    data_concated.to_csv(os.path.join(filepath, 'combined.csv'),index=False)
+    try:
+        dataframes = []
+        for file in os.listdir(filepath):
+            if str(file).endswith('csv'):
+                if not str(file).lower().startswith('combined'):
+                    environment = ''
+                    if str(file).lower().startswith('na'):
+                        environment = 'NA Prod'
+                    elif str(file).lower().startswith('asia'):
+                        environment = 'Asia Prod'
+                    df = pd.read_csv(os.path.join(filepath, file), usecols=['Source IP'])
+                    df['Environment'] = environment
+                    dataframes.append(df)
+        data_concated = pd.concat(dataframes, axis=0)
+        data_concated = data_concated.reset_index()
+        data_concated = data_concated.drop(['index'], axis=1)
+        data_concated = data_concated.drop_duplicates()
+        data_concated.to_csv(os.path.join(filepath, 'combined.csv'),index=False)
+    except Exception as err:
+        raise err
 
 if __name__  == '__main__':
     if len(sys.argv)==1:
